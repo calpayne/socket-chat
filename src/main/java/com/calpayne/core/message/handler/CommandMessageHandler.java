a2361fd8fef1adb01e976ee0253c7b7bcddd9a50
calpayne <34384159+calpayne@users.noreply.github.com>
2018-12-20 20:49:37 +0000

    Fixed /emoji

--- src/main/java/com/calpayne/core/message/handler/CommandMessageHandler.java
+++ src/main/java/com/calpayne/core/message/handler/CommandMessageHandler.java
@@ -28,16 +28,18 @@ public class CommandMessageHandler implements MessageHandler {
                         + "<b>/message handle message</b> - send a private message"));
                 break;
             case "/emoji":
-                server.sendMessage(new Message(MessageType.SERVER, "Server", message.getFrom(), "Emoji list:<br />"
-                        + ":alien: - <b>:alien:</b><br />"
-                        + ":banana: - <b>:banana:</b><br />"
-                        + ":cheers: - <b>:cheers:</b><br />"
-                        + ":disco: - <b>:disco:</b><br />"
-                        + ":fire: - <b>:fire:</b><br />"
-                        + ":ghost: - <b>:ghost:</b><br />"
-                        + ":heart: - <b>:heart:</b><br />"
-                        + ":star: - <b>:star:</b><br />"
-                        + ":trophy: - <b>:trophy:</b>"));
+                Message emojis = new Message(MessageType.SERVER, "Server", message.getFrom(), "Emoji list:<br />"
+                        + ":alien: is <b>:alien:</b><br />"
+                        + ":banana: is <b>:banana:</b><br />"
+                        + ":cheers: is <b>:cheers:</b><br />"
+                        + ":disco: is <b>:disco:</b><br />"
+                        + ":fire: is <b>:fire:</b><br />"
+                        + ":ghost: is <b>:ghost:</b><br />"
+                        + ":heart: is <b>:heart:</b><br />"
+                        + ":star: is <b>:star:</b><br />"
+                        + ":trophy: is <b>:trophy:</b>");
+                emojis.setOnlyFirstEmoji(true);
+                server.sendMessage(emojis);
                 break;
             case "/message":
                 String whipser = "";
